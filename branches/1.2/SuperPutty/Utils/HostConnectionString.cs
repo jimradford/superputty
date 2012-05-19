30e1086fa37a420238cd747bb936192a73a8b565
btateyama@gmail.com <btateyama@gmail.com@8d6e282c-8220-11de-b77e-d9b456c0130a>
2012-05-19 16:59:33 +0000

    Fixed CLI port parsing problem; added support for host connection strings on CLI and connection bar (e.g. ssh://localhost:3333)

---
+++ branches/1.2/SuperPutty/Utils/HostConnectionString.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperPutty.Data;
+using log4net;
+
+namespace SuperPutty.Utils
+{
+    /// <summary>
+    /// Helper class to parse host connection strings (e.g. ssh://localhost:2222)
+    /// </summary>
+    public class HostConnectionString
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(HostConnectionString));
+
+        public HostConnectionString(string hostString)
+        {
+
+            int idx = hostString.IndexOf("://");
+            string hostPort = hostString;
+            if (idx != -1)
+            {
+                // ssh://localhost:2020
+                this.Protocol = (ConnectionProtocol)Enum.Parse(typeof(ConnectionProtocol), hostString.Substring(0, idx), true);
+                hostPort = hostString.Substring(idx + 3);
+            }
+
+            int idxPort = hostPort.IndexOf(":");
+            if (idxPort != -1)
+            {
+                // localhost:2020
+                this.Host = hostPort.Substring(0, idxPort);
+                this.Port = Convert.ToInt32(hostPort.Substring(idxPort + 1));
+            }
+            else
+            {
+                // localhost
+                this.Host = hostPort;
+            }
+
+
+            log.InfoFormat(
+                "Parsed[{0}]: proto={1}, host={2}, port={3}", 
+                hostString, 
+                this.Protocol.HasValue ? this.Protocol.ToString() : "", 
+                this.Host, 
+                this.Port.HasValue ? this.Port.ToString() : "");
+        }
+
+        public ConnectionProtocol? Protocol { get; set; }
+        public string Host { get; set; }
+        public int? Port { get; set; }
+    }
+}
