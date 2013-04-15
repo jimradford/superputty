a99a7889e25849cab9193643cbf6858bdd967706
btateyama@gmail.com <btateyama@gmail.com>
2013-04-15 10:36:43 +0000

    310: Hide search (Added option to hide)

--- SuperPutty/SessionTreeview.Designer.cs
+++ SuperPutty/SessionTreeview.Designer.cs
@@ -72,13 +72,13 @@
             this.treeView1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.treeView1.ImageIndex = 0;
             this.treeView1.ImageList = this.imageList1;
-            this.treeView1.Location = new System.Drawing.Point(0, 26);
-            this.treeView1.Margin = new System.Windows.Forms.Padding(4);
+            this.treeView1.Location = new System.Drawing.Point(0, 32);
+            this.treeView1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.treeView1.Name = "treeView1";
             this.treeView1.SelectedImageIndex = 0;
             this.treeView1.ShowLines = false;
             this.treeView1.ShowRootLines = false;
-            this.treeView1.Size = new System.Drawing.Size(325, 383);
+            this.treeView1.Size = new System.Drawing.Size(433, 471);
             this.treeView1.TabIndex = 0;
             this.treeView1.ItemDrag += new System.Windows.Forms.ItemDragEventHandler(this.treeView1_ItemDrag);
             this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
@@ -109,72 +109,72 @@
             this.fileBrowserToolStripMenuItem});
             this.contextMenuStripAddTreeItem.Name = "contextMenuStripAddTreeItem";
             this.contextMenuStripAddTreeItem.ShowImageMargin = false;
-            this.contextMenuStripAddTreeItem.Size = new System.Drawing.Size(182, 192);
+            this.contextMenuStripAddTreeItem.Size = new System.Drawing.Size(216, 208);
             this.contextMenuStripAddTreeItem.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStripAddTreeItem_Opening);
             // 
             // newSessionToolStripMenuItem
             // 
             this.newSessionToolStripMenuItem.Name = "newSessionToolStripMenuItem";
-            this.newSessionToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.newSessionToolStripMenuItem.Size = new System.Drawing.Size(215, 24);
             this.newSessionToolStripMenuItem.Text = "New";
             this.newSessionToolStripMenuItem.Click += new System.EventHandler(this.CreateOrEditSessionToolStripMenuItem_Click);
             // 
             // createLikeToolStripMenuItem
             // 
             this.createLikeToolStripMenuItem.Name = "createLikeToolStripMenuItem";
-            this.createLikeToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.createLikeToolStripMenuItem.Size = new System.Drawing.Size(215, 24);
             this.createLikeToolStripMenuItem.Text = "Copy As";
             this.createLikeToolStripMenuItem.Click += new System.EventHandler(this.CreateOrEditSessionToolStripMenuItem_Click);
             // 
             // settingsToolStripMenuItem
             // 
             this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
-            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(215, 24);
             this.settingsToolStripMenuItem.Text = "Edit";
             this.settingsToolStripMenuItem.Click += new System.EventHandler(this.CreateOrEditSessionToolStripMenuItem_Click);
             // 
             // deleteToolStripMenuItem
             // 
             this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
-            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(215, 24);
             this.deleteToolStripMenuItem.Text = "Delete";
             this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(178, 6);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(212, 6);
             // 
             // connectToolStripMenuItem
             // 
             this.connectToolStripMenuItem.Name = "connectToolStripMenuItem";
-            this.connectToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.connectToolStripMenuItem.Size = new System.Drawing.Size(215, 24);
             this.connectToolStripMenuItem.Text = "Connect";
             this.connectToolStripMenuItem.Click += new System.EventHandler(this.connectToolStripMenuItem_Click);
             // 
             // connectExternalToolStripMenuItem
             // 
             this.connectExternalToolStripMenuItem.Name = "connectExternalToolStripMenuItem";
-            this.connectExternalToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.connectExternalToolStripMenuItem.Size = new System.Drawing.Size(215, 24);
             this.connectExternalToolStripMenuItem.Text = "Connect External";
             this.connectExternalToolStripMenuItem.Click += new System.EventHandler(this.connectExternalToolStripMenuItem_Click);
             // 
             // connectInNewSuperPuTTYToolStripMenuItem
             // 
             this.connectInNewSuperPuTTYToolStripMenuItem.Name = "connectInNewSuperPuTTYToolStripMenuItem";
-            this.connectInNewSuperPuTTYToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.connectInNewSuperPuTTYToolStripMenuItem.Size = new System.Drawing.Size(215, 24);
             this.connectInNewSuperPuTTYToolStripMenuItem.Text = "Connect in New Instance";
             this.connectInNewSuperPuTTYToolStripMenuItem.Click += new System.EventHandler(this.connectInNewSuperPuTTYToolStripMenuItem_Click);
             // 
             // toolStripSeparator2
             // 
             this.toolStripSeparator2.Name = "toolStripSeparator2";
-            this.toolStripSeparator2.Size = new System.Drawing.Size(178, 6);
+            this.toolStripSeparator2.Size = new System.Drawing.Size(212, 6);
             // 
             // fileBrowserToolStripMenuItem
             // 
             this.fileBrowserToolStripMenuItem.Name = "fileBrowserToolStripMenuItem";
-            this.fileBrowserToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.fileBrowserToolStripMenuItem.Size = new System.Drawing.Size(215, 24);
             this.fileBrowserToolStripMenuItem.Text = "File Transfer";
             this.fileBrowserToolStripMenuItem.Click += new System.EventHandler(this.fileBrowserToolStripMenuItem_Click);
             // 
@@ -194,75 +194,75 @@
             this.collapseAllToolStripMenuItem});
             this.contextMenuStripFolder.Name = "contextMenuStripAddTreeItem";
             this.contextMenuStripFolder.ShowImageMargin = false;
-            this.contextMenuStripFolder.Size = new System.Drawing.Size(129, 182);
+            this.contextMenuStripFolder.Size = new System.Drawing.Size(154, 196);
             this.contextMenuStripFolder.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStripFolder_Opening);
             // 
             // newSessionToolStripMenuItem1
             // 
             this.newSessionToolStripMenuItem1.Name = "newSessionToolStripMenuItem1";
-            this.newSessionToolStripMenuItem1.Size = new System.Drawing.Size(128, 22);
+            this.newSessionToolStripMenuItem1.Size = new System.Drawing.Size(153, 24);
             this.newSessionToolStripMenuItem1.Text = "New";
             this.newSessionToolStripMenuItem1.Click += new System.EventHandler(this.CreateOrEditSessionToolStripMenuItem_Click);
             // 
             // toolStripSeparator3
             // 
             this.toolStripSeparator3.Name = "toolStripSeparator3";
-            this.toolStripSeparator3.Size = new System.Drawing.Size(125, 6);
+            this.toolStripSeparator3.Size = new System.Drawing.Size(150, 6);
             // 
             // newFolderToolStripMenuItem
             // 
             this.newFolderToolStripMenuItem.Name = "newFolderToolStripMenuItem";
-            this.newFolderToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.newFolderToolStripMenuItem.Size = new System.Drawing.Size(153, 24);
             this.newFolderToolStripMenuItem.Text = "New Folder";
             this.newFolderToolStripMenuItem.Click += new System.EventHandler(this.newFolderToolStripMenuItem_Click);
             // 
             // removeFolderToolStripMenuItem
             // 
             this.removeFolderToolStripMenuItem.Name = "removeFolderToolStripMenuItem";
-            this.removeFolderToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.removeFolderToolStripMenuItem.Size = new System.Drawing.Size(153, 24);
             this.removeFolderToolStripMenuItem.Text = "Remove Folder";
             this.removeFolderToolStripMenuItem.Click += new System.EventHandler(this.removeFolderToolStripMenuItem_Click);
             // 
             // toolStripMenuItem1
             // 
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(125, 6);
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(150, 6);
             // 
             // renameToolStripMenuItem
             // 
             this.renameToolStripMenuItem.Name = "renameToolStripMenuItem";
-            this.renameToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.renameToolStripMenuItem.Size = new System.Drawing.Size(153, 24);
             this.renameToolStripMenuItem.Text = "Rename";
             this.renameToolStripMenuItem.Click += new System.EventHandler(this.renameToolStripMenuItem_Click);
             // 
             // toolStripMenuItem3
             // 
             this.toolStripMenuItem3.Name = "toolStripMenuItem3";
-            this.toolStripMenuItem3.Size = new System.Drawing.Size(125, 6);
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(150, 6);
             // 
             // connectAllToolStripMenuItem
             // 
             this.connectAllToolStripMenuItem.Name = "connectAllToolStripMenuItem";
-            this.connectAllToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.connectAllToolStripMenuItem.Size = new System.Drawing.Size(153, 24);
             this.connectAllToolStripMenuItem.Text = "Connect All";
             this.connectAllToolStripMenuItem.Click += new System.EventHandler(this.connectAllToolStripMenuItem_Click);
             // 
             // toolStripMenuItem2
             // 
             this.toolStripMenuItem2.Name = "toolStripMenuItem2";
-            this.toolStripMenuItem2.Size = new System.Drawing.Size(125, 6);
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(150, 6);
             // 
             // expandAllToolStripMenuItem
             // 
             this.expandAllToolStripMenuItem.Name = "expandAllToolStripMenuItem";
-            this.expandAllToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.expandAllToolStripMenuItem.Size = new System.Drawing.Size(153, 24);
             this.expandAllToolStripMenuItem.Text = "Expand All";
             this.expandAllToolStripMenuItem.Click += new System.EventHandler(this.expandAllToolStripMenuItem_Click);
             // 
             // collapseAllToolStripMenuItem
             // 
             this.collapseAllToolStripMenuItem.Name = "collapseAllToolStripMenuItem";
-            this.collapseAllToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.collapseAllToolStripMenuItem.Size = new System.Drawing.Size(153, 24);
             this.collapseAllToolStripMenuItem.Text = "Collapse All";
             this.collapseAllToolStripMenuItem.Click += new System.EventHandler(this.collapseAllToolStripMenuItem_Click);
             // 
@@ -278,17 +278,19 @@
             this.panelSearch.Controls.Add(this.btnClear);
             this.panelSearch.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelSearch.Location = new System.Drawing.Point(0, 0);
+            this.panelSearch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panelSearch.Name = "panelSearch";
-            this.panelSearch.Padding = new System.Windows.Forms.Padding(2);
-            this.panelSearch.Size = new System.Drawing.Size(325, 26);
+            this.panelSearch.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panelSearch.Size = new System.Drawing.Size(433, 32);
             this.panelSearch.TabIndex = 2;
             // 
             // txtSearch
             // 
             this.txtSearch.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.txtSearch.Location = new System.Drawing.Point(2, 2);
+            this.txtSearch.Location = new System.Drawing.Point(3, 2);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(273, 19);
+            this.txtSearch.Size = new System.Drawing.Size(363, 22);
             this.txtSearch.TabIndex = 0;
             this.txtSearch.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtSearch_KeyDown);
             // 
@@ -296,10 +298,10 @@
             // 
             this.btnSearch.Dock = System.Windows.Forms.DockStyle.Right;
             this.btnSearch.Image = global::SuperPutty.Properties.Resources.find;
-            this.btnSearch.Location = new System.Drawing.Point(275, 2);
+            this.btnSearch.Location = new System.Drawing.Point(366, 2);
             this.btnSearch.Margin = new System.Windows.Forms.Padding(0);
             this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(24, 22);
+            this.btnSearch.Size = new System.Drawing.Size(32, 28);
             this.btnSearch.TabIndex = 1;
             this.btnSearch.UseVisualStyleBackColor = true;
             this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
@@ -308,24 +310,24 @@
             // 
             this.btnClear.Dock = System.Windows.Forms.DockStyle.Right;
             this.btnClear.Image = global::SuperPutty.Properties.Resources.cross_black;
-            this.btnClear.Location = new System.Drawing.Point(299, 2);
+            this.btnClear.Location = new System.Drawing.Point(398, 2);
             this.btnClear.Margin = new System.Windows.Forms.Padding(0);
             this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(24, 22);
+            this.btnClear.Size = new System.Drawing.Size(32, 28);
             this.btnClear.TabIndex = 2;
             this.btnClear.UseVisualStyleBackColor = true;
             this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
             // SessionTreeview
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(325, 409);
+            this.ClientSize = new System.Drawing.Size(433, 503);
             this.Controls.Add(this.treeView1);
             this.Controls.Add(this.panelSearch);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "SessionTreeview";
             this.ShowIcon = false;
             this.TabText = "Sessions";
