c176993cce0e903fa529ad0d6299f4aa58d64b28
btateyama@gmail.com <btateyama@gmail.com@8d6e282c-8220-11de-b77e-d9b456c0130a>
2013-05-22 01:57:39 +0000

    Resize

--- branches/1.4/SuperPutty/AboutBox1.Designer.cs
+++ branches/1.4/SuperPutty/AboutBox1.Designer.cs
@@ -73,9 +73,9 @@
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 150F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 180F));
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel.Size = new System.Drawing.Size(417, 293);
+            this.tableLayoutPanel.Size = new System.Drawing.Size(543, 328);
             this.tableLayoutPanel.TabIndex = 0;
             // 
             // logoPictureBox
@@ -85,7 +85,7 @@
             this.logoPictureBox.Location = new System.Drawing.Point(3, 3);
             this.logoPictureBox.Name = "logoPictureBox";
             this.tableLayoutPanel.SetRowSpan(this.logoPictureBox, 7);
-            this.logoPictureBox.Size = new System.Drawing.Size(131, 287);
+            this.logoPictureBox.Size = new System.Drawing.Size(173, 322);
             this.logoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.logoPictureBox.TabIndex = 12;
             this.logoPictureBox.TabStop = false;
@@ -94,11 +94,11 @@
             // 
             this.labelProductName.Dock = System.Windows.Forms.DockStyle.Fill;
             this.labelProductName.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelProductName.Location = new System.Drawing.Point(143, 0);
+            this.labelProductName.Location = new System.Drawing.Point(185, 0);
             this.labelProductName.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
             this.labelProductName.MaximumSize = new System.Drawing.Size(0, 17);
             this.labelProductName.Name = "labelProductName";
-            this.labelProductName.Size = new System.Drawing.Size(271, 17);
+            this.labelProductName.Size = new System.Drawing.Size(355, 17);
             this.labelProductName.TabIndex = 19;
             this.labelProductName.Text = "Product Name";
             this.labelProductName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -107,11 +107,11 @@
             // 
             this.labelVersion.Dock = System.Windows.Forms.DockStyle.Fill;
             this.labelVersion.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelVersion.Location = new System.Drawing.Point(143, 22);
+            this.labelVersion.Location = new System.Drawing.Point(185, 23);
             this.labelVersion.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
             this.labelVersion.MaximumSize = new System.Drawing.Size(0, 17);
             this.labelVersion.Name = "labelVersion";
-            this.labelVersion.Size = new System.Drawing.Size(271, 17);
+            this.labelVersion.Size = new System.Drawing.Size(355, 17);
             this.labelVersion.TabIndex = 0;
             this.labelVersion.Text = "Version";
             this.labelVersion.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -120,11 +120,11 @@
             // 
             this.labelCopyright.Dock = System.Windows.Forms.DockStyle.Fill;
             this.labelCopyright.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelCopyright.Location = new System.Drawing.Point(143, 44);
+            this.labelCopyright.Location = new System.Drawing.Point(185, 46);
             this.labelCopyright.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
             this.labelCopyright.MaximumSize = new System.Drawing.Size(0, 17);
             this.labelCopyright.Name = "labelCopyright";
-            this.labelCopyright.Size = new System.Drawing.Size(271, 17);
+            this.labelCopyright.Size = new System.Drawing.Size(355, 17);
             this.labelCopyright.TabIndex = 21;
             this.labelCopyright.Text = "Copyright";
             this.labelCopyright.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -133,7 +133,7 @@
             // 
             this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.okButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.okButton.Location = new System.Drawing.Point(339, 267);
+            this.okButton.Location = new System.Drawing.Point(465, 302);
             this.okButton.Name = "okButton";
             this.okButton.Size = new System.Drawing.Size(75, 23);
             this.okButton.TabIndex = 24;
@@ -146,10 +146,10 @@
             this.tabControl1.Controls.Add(this.tabPage3);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tabControl1.Location = new System.Drawing.Point(140, 113);
+            this.tabControl1.Location = new System.Drawing.Point(182, 118);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(274, 144);
+            this.tabControl1.Size = new System.Drawing.Size(358, 174);
             this.tabControl1.TabIndex = 25;
             // 
             // tabPage1
@@ -158,7 +158,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 24);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(266, 116);
+            this.tabPage1.Size = new System.Drawing.Size(350, 146);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "About";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -174,12 +174,12 @@
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(260, 110);
+            this.textBox1.Size = new System.Drawing.Size(344, 140);
             this.textBox1.TabIndex = 24;
             this.textBox1.TabStop = false;
             this.textBox1.Text = "SuperPutty is a tabbed interface for the PuTTY SSH Client. It is can be used for " +
-                "both terminal sessions and file transfers both utilizing the secure SSH protocol" +
-                ".";
+    "both terminal sessions and file transfers both utilizing the secure SSH protocol" +
+    ".";
             // 
             // tabPage2
             // 
@@ -187,7 +187,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 24);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(318, 116);
+            this.tabPage2.Size = new System.Drawing.Size(350, 166);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "License";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -204,7 +204,7 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
             this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox2.Size = new System.Drawing.Size(312, 110);
+            this.textBox2.Size = new System.Drawing.Size(344, 160);
             this.textBox2.TabIndex = 25;
             this.textBox2.TabStop = false;
             this.textBox2.Text = resources.GetString("textBox2.Text");
@@ -214,7 +214,7 @@
             this.tabPage3.Controls.Add(this.richTextBox1);
             this.tabPage3.Location = new System.Drawing.Point(4, 24);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(318, 116);
+            this.tabPage3.Size = new System.Drawing.Size(350, 146);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Acknowledgements";
             this.tabPage3.UseVisualStyleBackColor = true;
@@ -228,7 +228,7 @@
             this.richTextBox1.Location = new System.Drawing.Point(0, 0);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.ReadOnly = true;
-            this.richTextBox1.Size = new System.Drawing.Size(318, 116);
+            this.richTextBox1.Size = new System.Drawing.Size(350, 146);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
             this.richTextBox1.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.richTextBox1_LinkClicked);
@@ -237,10 +237,10 @@
             // 
             this.linkLabelCompany.AutoSize = true;
             this.linkLabelCompany.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.linkLabelCompany.Location = new System.Drawing.Point(143, 66);
+            this.linkLabelCompany.Location = new System.Drawing.Point(185, 69);
             this.linkLabelCompany.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
             this.linkLabelCompany.Name = "linkLabelCompany";
-            this.linkLabelCompany.Size = new System.Drawing.Size(271, 22);
+            this.linkLabelCompany.Size = new System.Drawing.Size(355, 23);
             this.linkLabelCompany.TabIndex = 26;
             this.linkLabelCompany.TabStop = true;
             this.linkLabelCompany.Text = "linkLabelCompany";
@@ -251,10 +251,10 @@
             // 
             this.linkLabelCompany2.AutoSize = true;
             this.linkLabelCompany2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.linkLabelCompany2.Location = new System.Drawing.Point(143, 88);
+            this.linkLabelCompany2.Location = new System.Drawing.Point(185, 92);
             this.linkLabelCompany2.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
             this.linkLabelCompany2.Name = "linkLabelCompany2";
-            this.linkLabelCompany2.Size = new System.Drawing.Size(271, 22);
+            this.linkLabelCompany2.Size = new System.Drawing.Size(355, 23);
             this.linkLabelCompany2.TabIndex = 27;
             this.linkLabelCompany2.TabStop = true;
             this.linkLabelCompany2.Text = "linkLabelCompany2";
@@ -266,7 +266,7 @@
             this.AcceptButton = this.okButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(435, 311);
+            this.ClientSize = new System.Drawing.Size(561, 346);
             this.Controls.Add(this.tableLayoutPanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
