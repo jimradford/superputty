7c413ac5f72700a5a766725c382248231a25d33d
Jim Radford <jradford@npl.com>
2015-09-16 17:46:44 -0700

    Revert "Improved desktop UI performance"

--- SuperPutty/SuperPuTTY.cs
+++ SuperPutty/SuperPuTTY.cs
@@ -96,7 +96,6 @@ namespace SuperPutty
 
             // Register IpcChanncel for single instance support
             SingleInstanceHelper.RegisterRemotingService();
-            WindowEvents = new GlobalWindowEvents();
 
             Log.Info("Initialized");
         }
@@ -725,7 +724,6 @@ namespace SuperPutty
         public static BindingList<SessionData> Sessions { get { return sessionsList; } }
         public static CommandLineOptions CommandLine { get; private set; }
         public static ImageList Images { get; private set; }
-        public static GlobalWindowEvents WindowEvents { get; private set; }
 
         /// <summary>true of KiTTY is being used instead of putty</summary>
         public static bool IsKiTTY
@@ -739,17 +737,6 @@ namespace SuperPutty
                     isKitty = exe != null && exe.ToLower().StartsWith("kitty");
                 }
                 return isKitty;
-            }
-        }
-
-        public static string PuTTYAppName
-        {
-            get
-            {
-                if (IsKiTTY)
-                    return "KiTTY";
-
-                return "PuTTY";
             }
         }
 
