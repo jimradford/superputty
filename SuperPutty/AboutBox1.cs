jimradford/superputty

commit ba7bd81ed17563eb89ae95c39722b30847ba3fd9

Fix compiling issues under vs2013.
Signed-off-by: Colin Xu <colin.xu@gmail.com>

SuperPutty/AboutBox1.cs
@@ -68,7 +68,7 @@ namespace SuperPutty
             }
         }
 
-        public string AssemblyVersion => Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        public string AssemblyVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
 
         public string AssemblyDescription
         {

SuperPutty/Data/LayoutData.cs
@@ -12,6 +12,7 @@ namespace SuperPutty.Data
         {
             this.FilePath = filePath;
             this.Name = Path.GetFileNameWithoutExtension(filePath);
+            this.IsDefault = (this.Name == SuperPuTTY.Settings.DefaultLayoutName);
         }
 
         public string Name { get; set; }
@@ -19,7 +20,7 @@ namespace SuperPutty.Data
 
         public bool IsReadOnly { get; set; }
 
-        public bool IsDefault => this.Name == SuperPuTTY.Settings.DefaultLayoutName;
+        public bool IsDefault;
 
         public override string ToString()
         {

SuperPutty/Gui/DataGridViewProgressColumn.cs
@@ -19,10 +19,12 @@ namespace SuperPutty.Gui
     public class DataGridViewProgressColumn : DataGridViewImageColumn
     {
         public static Color _ProgressBarColor;
+        private DataGridViewProgressCell ProgressBarCellTemplate;
 
         public DataGridViewProgressColumn()
         {
             CellTemplate = new DataGridViewProgressCell();
+            this.ProgressBarCellTemplate = (DataGridViewProgressCell)CellTemplate;
         }
 
         public override DataGridViewCell CellTemplate
@@ -84,9 +86,6 @@ namespace SuperPutty.Gui
                 }
             }
         }
-
-
-        private DataGridViewProgressCell ProgressBarCellTemplate => (DataGridViewProgressCell)this.CellTemplate;
     }
 
     class DataGridViewProgressCell : DataGridViewImageCell

SuperPutty/SuperPuTTY.cs
@@ -58,6 +58,11 @@ namespace SuperPutty
 
         public static void Initialize(string[] args)
         {
+            IsScpEnabled = File.Exists(SuperPuTTY.Settings.PscpExe);
+            IsFilezillaEnabled = File.Exists(SuperPuTTY.Settings.FileZillaExe);
+            IsWinSCPEnabled = File.Exists(SuperPuTTY.Settings.WinSCPExe);
+            SessionsFileName = Path.Combine(Settings.SettingsFolder, "Sessions.XML");
+
             Log.InfoFormat(
                 "Initializing.  Version={0}, UserSettings={1}, SettingsFolder={2}", 
                 Version, Settings.SettingsFilePath, Settings.SettingsFolder);
@@ -303,7 +308,7 @@ namespace SuperPutty
         #region Sessions
 
         /// <summary>Returns A string containing the path to the saved sessions database on disk</summary>
-        private static string SessionsFileName => Path.Combine(Settings.SettingsFolder, "Sessions.XML");
+        private static string SessionsFileName;
 
         /// <summary>Load sessions database from file into the application</summary>
         public static void LoadSessions()
@@ -755,25 +760,25 @@ namespace SuperPutty
         }
 
         /// <summary>true if the application has not defined where the putty scp program is located</summary>
-        public static bool IsScpEnabled => File.Exists(SuperPuTTY.Settings.PscpExe);
+        public static bool IsScpEnabled;
 
         /// <summary>true if the application has defined where the filezilla program is located</summary>
-        public static bool IsFilezillaEnabled => File.Exists(SuperPuTTY.Settings.FileZillaExe);
+        public static bool IsFilezillaEnabled;
 
         /// <summary>true if the application has defined where the winSCP program is located</summary>
-        public static bool IsWinSCPEnabled => File.Exists(SuperPuTTY.Settings.WinSCPExe);
+        public static bool IsWinSCPEnabled;
 
         /// <summary>Returns a string containing the current version of SuperPuTTY</summary>
-        public static string Version => Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        public static string Version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
 
-        internal static Settings Settings => Settings.Default;
+        internal static Settings Settings = Settings.Default;
         public static frmSuperPutty MainForm { get; set; }        
-        public static string LayoutsDir => Path.Combine(Settings.SettingsFolder, "layouts");
+        public static string LayoutsDir = Path.Combine(Settings.SettingsFolder, "layouts");
         public static LayoutData CurrentLayout { get; private set; }
         public static LayoutData StartingLayout { get; private set; }
         public static SessionDataStartInfo StartingSession { get; private set; }
-        public static BindingList<LayoutData> Layouts => layouts;
-        public static BindingList<SessionData> Sessions => sessionsList;
+        public static BindingList<LayoutData> Layouts = layouts;
+        public static BindingList<SessionData> Sessions = sessionsList;
         public static CommandLineOptions CommandLine { get; private set; }
         public static ImageList Images { get; private set; }
         public static GlobalWindowEvents WindowEvents { get; private set; }
@@ -805,7 +810,7 @@ namespace SuperPutty
         }
 
         /// <summary>The path to the default AutoRestore layout configuration</summary>
-        public static string AutoRestoreLayoutPath => Path.Combine(Settings.SettingsFolder, LayoutData.AutoRestoreLayoutFileName);
+        public static string AutoRestoreLayoutPath = Path.Combine(Settings.SettingsFolder, LayoutData.AutoRestoreLayoutFileName);
 
         #endregion
     }

SuperPutty/Utils/GlobalHotKey.cs
@@ -11,6 +11,9 @@ namespace SuperPutty.Utils
         {
             this.Form = form;
             this.Shortcut = shortcut;
+            this.IsControlSet = IsSet(this.Shortcut.Modifiers, Keys.Control);
+            this.IsAltSet = IsSet(this.Shortcut.Modifiers, Keys.Alt);
+            this.IsShiftSet = IsSet(this.Shortcut.Modifiers, Keys.Shift);
 
             // convert the Keys to modifiers
             this.Modifiers = NativeMethods.HotKeysConstants.NOMOD;
@@ -48,9 +51,9 @@ namespace SuperPutty.Utils
             return modifiers.Any(modifier => (keys & modifier) == modifier);
         }
 
-        public bool IsControlSet => IsSet(this.Shortcut.Modifiers, Keys.Control);
-        public bool IsAltSet => IsSet(this.Shortcut.Modifiers, Keys.Alt);
-        public bool IsShiftSet => IsSet(this.Shortcut.Modifiers, Keys.Shift);
+        public bool IsControlSet;
+        public bool IsAltSet;
+        public bool IsShiftSet;
 
         public KeyboardShortcut Shortcut { get; private set; }
         public Form Form { get; private set; }

SuperPutty/Utils/SingletonToolWindowHelper.cs
@@ -22,6 +22,8 @@ namespace SuperPutty.Utils
             this.DockPanel = dockPanel;
             this.Initializer = initializer;
             this.InitializerResource = InitializerResource;
+            this.IsVisibleAsToolWindow = (this.Instance != null && this.Instance.DockHandler.Pane != null && !this.Instance.DockHandler.Pane.IsAutoHide);
+            this.IsVisible = (this.Instance != null && this.Instance.Visible);
         }
 
         public void ShowWindow(DockState dockState)
@@ -65,7 +67,7 @@ namespace SuperPutty.Utils
             SuperPuTTY.ReportStatus("Showing " + this.Name);
         }
 
-        public bool IsVisibleAsToolWindow => this.Instance != null && this.Instance.DockHandler.Pane != null && !this.Instance.DockHandler.Pane.IsAutoHide;
+        public bool IsVisibleAsToolWindow;
 
         public T Initialize()
         {
@@ -101,7 +103,7 @@ namespace SuperPutty.Utils
             }
         }
 
-        public bool IsVisible => this.Instance != null && this.Instance.Visible;
+        public bool IsVisible;
 
 
         public string Name { get; private set; }

SuperPutty/Utils/SortableBindingList.cs
@@ -34,7 +34,7 @@ namespace SuperPutty.Utils
     /// <typeparam name="T"></typeparam>
     public class SortableBindingList<T> : BindingList<T>, IList<T>
     {
-        protected override bool SupportsSortingCore => true;
+        //protected bool SupportsSortingCore = true;
 
         protected override bool IsSortedCore
         {
@@ -77,15 +77,17 @@ namespace SuperPutty.Utils
         }
 
         private ListSortDirection sortDirection;
-        protected override ListSortDirection SortDirectionCore => sortDirection;
+        protected new ListSortDirection SortDirectionCore;
 
         private PropertyDescriptor sortProperty;
-        protected override PropertyDescriptor SortPropertyCore => sortProperty;
+        protected new PropertyDescriptor SortPropertyCore;
 
         protected override void ApplySortCore(PropertyDescriptor prop, ListSortDirection direction)
         {
             sortProperty = prop;
             sortDirection = direction;
+            SortDirectionCore = sortDirection;
+            SortPropertyCore = sortProperty;
 
             List<T> list = (List<T>)Items;
             list.Sort(delegate(T lhs, T rhs)
@@ -124,7 +126,9 @@ namespace SuperPutty.Utils
         protected override void RemoveSortCore()
         {
             sortDirection = ListSortDirection.Ascending;
+            SortDirectionCore = sortDirection;
             sortProperty = null;
+            SortPropertyCore = sortProperty;
         }
 
         /// <summary>Overrides call to EndNew fired when an item is added to the collection</summary>

SuperPutty/Utils/TabSwitcher.cs
@@ -33,21 +33,36 @@ namespace SuperPutty.Utils
     {
         private static readonly ILog Log = LogManager.GetLogger(typeof(TabSwitcher));
 
-        public static readonly ITabSwitchStrategy[] Strategies;
-        static TabSwitcher()
+        public static ITabSwitchStrategy[] Strategies;
+        public TabSwitcher()
         {
-            List<ITabSwitchStrategy> strats = new List<ITabSwitchStrategy>
+            this.Documents = this.tabSwitchStrategy.GetDocuments();
+            this.ActiveDocument = (ToolWindow)this.DockPanel.ActiveDocument;
+
+            if(Strategies == null)
             {
-                new VisualOrderTabSwitchStrategy(),
-                new OpenOrderTabSwitchStrategy(),
-                new MRUTabSwitchStrategy()
-            };
-            //strats.Add(new MRUTabSwitchStrategyOld());
-            Strategies = strats.ToArray();
+                List<ITabSwitchStrategy> strats = new List<ITabSwitchStrategy>
+                {
+                    new VisualOrderTabSwitchStrategy(),
+                    new OpenOrderTabSwitchStrategy(),
+                    new MRUTabSwitchStrategy()
+                };
+                Strategies = strats.ToArray();
+            }
         }
 
         public static ITabSwitchStrategy StrategyFromTypeName(String typeName)
         {
+            if(Strategies == null)
+            {
+                List<ITabSwitchStrategy> strats = new List<ITabSwitchStrategy>
+                {
+                    new VisualOrderTabSwitchStrategy(),
+                    new OpenOrderTabSwitchStrategy(),
+                    new MRUTabSwitchStrategy()
+                };
+                Strategies = strats.ToArray();
+            }
             ITabSwitchStrategy strategy = Strategies[0];
             try
             {
@@ -167,9 +182,8 @@ namespace SuperPutty.Utils
             }
         }
 
-        public IList<IDockContent> Documents => this.tabSwitchStrategy.GetDocuments();
-
-        public ToolWindow ActiveDocument => (ToolWindow)this.DockPanel.ActiveDocument;
+        public IList<IDockContent> Documents;
+        public ToolWindow ActiveDocument;
         public DockPanel DockPanel { get; private set; }
         public bool IsSwitchingTabs { get; set; }
 
@@ -181,7 +195,7 @@ namespace SuperPutty.Utils
     #region ITabSwitchStrategy
     public interface ITabSwitchStrategy : IDisposable
     {
-        string Description { get; }
+        string Description { get; }         
         IList<IDockContent> GetDocuments();
         void Initialize(DockPanel panel);
         void AddTab(ToolWindow tab);
@@ -306,12 +320,12 @@ namespace SuperPutty.Utils
     public class MRUTabSwitchStrategy : ITabSwitchStrategy
     {
         private static readonly ILog Log = LogManager.GetLogger(typeof(MRUTabSwitchStrategy));
-
-        public string Description => "MRU: Similar to Windows Alt-Tab";
+        public string Description { get; protected set; }
 
         public void Initialize(DockPanel panel)
         {
             this.DockPanel = panel;
+			Description = "MRU: Similar to Windows Alt-Tab";
         }
 
         public void AddTab(ToolWindow newTab)

commit 40c27a32a3367eef3d5b5a4ee1b1aabf5168617b

Fix regression caused by ba7bd81.
  Revert commit ba7bd81ed17563eb89ae95c39722b30847ba3fd9.
  Proper fix expression-bodied member compling under VS2013.

Signed-off-by: Colin Xu <colin.xu@gmail.com>

SuperPutty/AboutBox1.cs
@@ -68,7 +68,7 @@ namespace SuperPutty
             }
         }
 
-        public string AssemblyVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        public string AssemblyVersion { get { return Assembly.GetExecutingAssembly().GetName().Version.ToString(); } }
 
         public string AssemblyDescription
         {

SuperPutty/Data/LayoutData.cs
@@ -12,7 +12,6 @@ namespace SuperPutty.Data
         {
             this.FilePath = filePath;
             this.Name = Path.GetFileNameWithoutExtension(filePath);
-            this.IsDefault = (this.Name == SuperPuTTY.Settings.DefaultLayoutName);
         }
 
         public string Name { get; set; }
@@ -20,7 +19,7 @@ namespace SuperPutty.Data
 
         public bool IsReadOnly { get; set; }
 
-        public bool IsDefault;
+        public bool IsDefault { get { return this.Name == SuperPuTTY.Settings.DefaultLayoutName; } }
 
         public override string ToString()
         {

SuperPutty/Gui/DataGridViewProgressColumn.cs
@@ -19,12 +19,10 @@ namespace SuperPutty.Gui
     public class DataGridViewProgressColumn : DataGridViewImageColumn
     {
         public static Color _ProgressBarColor;
-        private DataGridViewProgressCell ProgressBarCellTemplate;
 
         public DataGridViewProgressColumn()
         {
             CellTemplate = new DataGridViewProgressCell();
-            this.ProgressBarCellTemplate = (DataGridViewProgressCell)CellTemplate;
         }
 
         public override DataGridViewCell CellTemplate
@@ -86,6 +84,9 @@ namespace SuperPutty.Gui
                 }
             }
         }
+
+
+        private DataGridViewProgressCell ProgressBarCellTemplate { get { return (DataGridViewProgressCell)this.CellTemplate; } } 
     }
 
     class DataGridViewProgressCell : DataGridViewImageCell

SuperPutty/SuperPuTTY.cs
@@ -58,11 +58,6 @@ namespace SuperPutty
 
         public static void Initialize(string[] args)
         {
-            IsScpEnabled = File.Exists(SuperPuTTY.Settings.PscpExe);
-            IsFilezillaEnabled = File.Exists(SuperPuTTY.Settings.FileZillaExe);
-            IsWinSCPEnabled = File.Exists(SuperPuTTY.Settings.WinSCPExe);
-            SessionsFileName = Path.Combine(Settings.SettingsFolder, "Sessions.XML");
-
             Log.InfoFormat(
                 "Initializing.  Version={0}, UserSettings={1}, SettingsFolder={2}", 
                 Version, Settings.SettingsFilePath, Settings.SettingsFolder);
@@ -308,7 +303,7 @@ namespace SuperPutty
         #region Sessions
 
         /// <summary>Returns A string containing the path to the saved sessions database on disk</summary>
-        private static string SessionsFileName;
+        private static string SessionsFileName { get { return Path.Combine(Settings.SettingsFolder, "Sessions.XML"); } }
 
         /// <summary>Load sessions database from file into the application</summary>
         public static void LoadSessions()
@@ -760,25 +755,25 @@ namespace SuperPutty
         }
 
         /// <summary>true if the application has not defined where the putty scp program is located</summary>
-        public static bool IsScpEnabled;
+        public static bool IsScpEnabled { get { return File.Exists(SuperPuTTY.Settings.PscpExe); } }
 
         /// <summary>true if the application has defined where the filezilla program is located</summary>
-        public static bool IsFilezillaEnabled;
+        public static bool IsFilezillaEnabled { get { return File.Exists(SuperPuTTY.Settings.FileZillaExe); } }
 
         /// <summary>true if the application has defined where the winSCP program is located</summary>
-        public static bool IsWinSCPEnabled;
+        public static bool IsWinSCPEnabled { get { return File.Exists(SuperPuTTY.Settings.WinSCPExe); } }
 
         /// <summary>Returns a string containing the current version of SuperPuTTY</summary>
-        public static string Version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        public static string Version { get { return Assembly.GetExecutingAssembly().GetName().Version.ToString(); } }
 
-        internal static Settings Settings = Settings.Default;
+        internal static Settings Settings { get { return Settings.Default; } }
         public static frmSuperPutty MainForm { get; set; }        
-        public static string LayoutsDir = Path.Combine(Settings.SettingsFolder, "layouts");
+        public static string LayoutsDir { get { return Path.Combine(Settings.SettingsFolder, "layouts"); } }
         public static LayoutData CurrentLayout { get; private set; }
         public static LayoutData StartingLayout { get; private set; }
         public static SessionDataStartInfo StartingSession { get; private set; }
-        public static BindingList<LayoutData> Layouts = layouts;
-        public static BindingList<SessionData> Sessions = sessionsList;
+        public static BindingList<LayoutData> Layouts { get { return layouts; } }
+        public static BindingList<SessionData> Sessions { get { return sessionsList; } }
         public static CommandLineOptions CommandLine { get; private set; }
         public static ImageList Images { get; private set; }
         public static GlobalWindowEvents WindowEvents { get; private set; }
@@ -810,7 +805,7 @@ namespace SuperPutty
         }
 
         /// <summary>The path to the default AutoRestore layout configuration</summary>
-        public static string AutoRestoreLayoutPath = Path.Combine(Settings.SettingsFolder, LayoutData.AutoRestoreLayoutFileName);
+        public static string AutoRestoreLayoutPath { get { return Path.Combine(Settings.SettingsFolder, LayoutData.AutoRestoreLayoutFileName); } }
 
         #endregion
     }

SuperPutty/SuperPutty.csproj
@@ -128,7 +128,6 @@
     </Compile>
     <Compile Include="Gui\QuickSelectorData.cs">
       <DependentUpon>QuickSelectorData.xsd</DependentUpon>
-      <SubType>Component</SubType>
     </Compile>
     <Compile Include="Gui\QuickSelectorData.Designer.cs">
       <AutoGen>True</AutoGen>

SuperPutty/Utils/GlobalHotKey.cs
@@ -11,9 +11,6 @@ namespace SuperPutty.Utils
         {
             this.Form = form;
             this.Shortcut = shortcut;
-            this.IsControlSet = IsSet(this.Shortcut.Modifiers, Keys.Control);
-            this.IsAltSet = IsSet(this.Shortcut.Modifiers, Keys.Alt);
-            this.IsShiftSet = IsSet(this.Shortcut.Modifiers, Keys.Shift);
 
             // convert the Keys to modifiers
             this.Modifiers = NativeMethods.HotKeysConstants.NOMOD;
@@ -51,9 +48,9 @@ namespace SuperPutty.Utils
             return modifiers.Any(modifier => (keys & modifier) == modifier);
         }
 
-        public bool IsControlSet;
-        public bool IsAltSet;
-        public bool IsShiftSet;
+        public bool IsControlSet { get { return IsSet(this.Shortcut.Modifiers, Keys.Control); } }
+        public bool IsAltSet { get { return IsSet(this.Shortcut.Modifiers, Keys.Alt); } }
+        public bool IsShiftSet { get { return IsSet(this.Shortcut.Modifiers, Keys.Shift); } }
 
         public KeyboardShortcut Shortcut { get; private set; }
         public Form Form { get; private set; }

SuperPutty/Utils/SingletonToolWindowHelper.cs
@@ -22,8 +22,6 @@ namespace SuperPutty.Utils
             this.DockPanel = dockPanel;
             this.Initializer = initializer;
             this.InitializerResource = InitializerResource;
-            this.IsVisibleAsToolWindow = (this.Instance != null && this.Instance.DockHandler.Pane != null && !this.Instance.DockHandler.Pane.IsAutoHide);
-            this.IsVisible = (this.Instance != null && this.Instance.Visible);
         }
 
         public void ShowWindow(DockState dockState)
@@ -67,7 +65,7 @@ namespace SuperPutty.Utils
             SuperPuTTY.ReportStatus("Showing " + this.Name);
         }
 
-        public bool IsVisibleAsToolWindow;
+        public bool IsVisibleAsToolWindow { get { return this.Instance != null && this.Instance.DockHandler.Pane != null && !this.Instance.DockHandler.Pane.IsAutoHide; } }
 
         public T Initialize()
         {
@@ -103,7 +101,7 @@ namespace SuperPutty.Utils
             }
         }
 
-        public bool IsVisible;
+        public bool IsVisible { get { return this.Instance != null && this.Instance.Visible; } }
 
 
         public string Name { get; private set; }

SuperPutty/Utils/SortableBindingList.cs
@@ -34,7 +34,7 @@ namespace SuperPutty.Utils
     /// <typeparam name="T"></typeparam>
     public class SortableBindingList<T> : BindingList<T>, IList<T>
     {
-        //protected bool SupportsSortingCore = true;
+        protected override bool SupportsSortingCore { get { return true; } }
 
         protected override bool IsSortedCore
         {
@@ -77,17 +77,15 @@ namespace SuperPutty.Utils
         }
 
         private ListSortDirection sortDirection;
-        protected new ListSortDirection SortDirectionCore;
+        protected override ListSortDirection SortDirectionCore { get { return sortDirection; } } 
 
         private PropertyDescriptor sortProperty;
-        protected new PropertyDescriptor SortPropertyCore;
+        protected override PropertyDescriptor SortPropertyCore { get { return sortProperty; } }
 
         protected override void ApplySortCore(PropertyDescriptor prop, ListSortDirection direction)
         {
             sortProperty = prop;
             sortDirection = direction;
-            SortDirectionCore = sortDirection;
-            SortPropertyCore = sortProperty;
 
             List<T> list = (List<T>)Items;
             list.Sort(delegate(T lhs, T rhs)
@@ -126,9 +124,7 @@ namespace SuperPutty.Utils
         protected override void RemoveSortCore()
         {
             sortDirection = ListSortDirection.Ascending;
-            SortDirectionCore = sortDirection;
             sortProperty = null;
-            SortPropertyCore = sortProperty;
         }
 
         /// <summary>Overrides call to EndNew fired when an item is added to the collection</summary>

SuperPutty/Utils/TabSwitcher.cs
@@ -33,36 +33,21 @@ namespace SuperPutty.Utils
     {
         private static readonly ILog Log = LogManager.GetLogger(typeof(TabSwitcher));
 
-        public static ITabSwitchStrategy[] Strategies;
-        public TabSwitcher()
+        public static readonly ITabSwitchStrategy[] Strategies;
+        static TabSwitcher()
         {
-            this.Documents = this.tabSwitchStrategy.GetDocuments();
-            this.ActiveDocument = (ToolWindow)this.DockPanel.ActiveDocument;
-
-            if(Strategies == null)
+            List<ITabSwitchStrategy> strats = new List<ITabSwitchStrategy>
             {
-                List<ITabSwitchStrategy> strats = new List<ITabSwitchStrategy>
-                {
-                    new VisualOrderTabSwitchStrategy(),
-                    new OpenOrderTabSwitchStrategy(),
-                    new MRUTabSwitchStrategy()
-                };
-                Strategies = strats.ToArray();
-            }
+                new VisualOrderTabSwitchStrategy(),
+                new OpenOrderTabSwitchStrategy(),
+                new MRUTabSwitchStrategy()
+            };
+            //strats.Add(new MRUTabSwitchStrategyOld());
+            Strategies = strats.ToArray();
         }
 
         public static ITabSwitchStrategy StrategyFromTypeName(String typeName)
         {
-            if(Strategies == null)
-            {
-                List<ITabSwitchStrategy> strats = new List<ITabSwitchStrategy>
-                {
-                    new VisualOrderTabSwitchStrategy(),
-                    new OpenOrderTabSwitchStrategy(),
-                    new MRUTabSwitchStrategy()
-                };
-                Strategies = strats.ToArray();
-            }
             ITabSwitchStrategy strategy = Strategies[0];
             try
             {
@@ -182,8 +167,9 @@ namespace SuperPutty.Utils
             }
         }
 
-        public IList<IDockContent> Documents;
-        public ToolWindow ActiveDocument;
+        public IList<IDockContent> Documents { get { return this.tabSwitchStrategy.GetDocuments(); } } 
+
+        public ToolWindow ActiveDocument { get { return (ToolWindow)this.DockPanel.ActiveDocument; } }
         public DockPanel DockPanel { get; private set; }
         public bool IsSwitchingTabs { get; set; }
 
@@ -195,7 +181,7 @@ namespace SuperPutty.Utils
     #region ITabSwitchStrategy
     public interface ITabSwitchStrategy : IDisposable
     {
-        string Description { get; }         
+        string Description { get; }
         IList<IDockContent> GetDocuments();
         void Initialize(DockPanel panel);
         void AddTab(ToolWindow tab);
@@ -320,12 +306,12 @@ namespace SuperPutty.Utils
     public class MRUTabSwitchStrategy : ITabSwitchStrategy
     {
         private static readonly ILog Log = LogManager.GetLogger(typeof(MRUTabSwitchStrategy));
-        public string Description { get; protected set; }
+
+        public string Description { get { return "MRU: Similar to Windows Alt-Tab"; } }
 
         public void Initialize(DockPanel panel)
         {
             this.DockPanel = panel;
-			Description = "MRU: Similar to Windows Alt-Tab";
         }
 
         public void AddTab(ToolWindow newTab)

SuperPutty/Utils/WindowActivator.cs
@@ -7,7 +7,7 @@ namespace SuperPutty.Utils
     #region WindowActivator
     public abstract class WindowActivator
     {
-        protected ILog Log = LogManager.GetLogger(typeof(WindowActivator).GetType());
+        protected ILog Log { get { return LogManager.GetLogger(this.GetType()); } }
 
         public abstract void ActivateForm(Form form, DesktopWindow window, IntPtr hwnd);
     } 

SuperPutty/ctlApplicationPanel.cs
@@ -67,12 +67,16 @@ namespace SuperPutty
 DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
         public string ApplicationWorkingDirectory { get; set; }
 
-        public IntPtr AppWindowHandle;
+        public IntPtr AppWindowHandle { get { return this.m_AppWin; } } 
 
         #endregion
 
         public ApplicationPanel()
         {
+            this.ApplicationName = "";
+            this.ApplicationParameters = "";
+            this.ApplicationWorkingDirectory = "";
+
             this.Disposed += new EventHandler(ApplicationPanel_Disposed);
             SuperPuTTY.LayoutChanged += new EventHandler<Data.LayoutChangedEventArgs>(SuperPuTTY_LayoutChanged);
 
@@ -83,11 +87,6 @@ DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
             //this.m_windowActivator = new SetFGCombinedWindowActivator();
             SuperPuTTY.Settings.SettingsSaving += Settings_SettingsSaving;
             SuperPuTTY.WindowEvents.SystemSwitch += new EventHandler<GlobalWindowEventArgs>(OnSystemSwitch);
-            this.ApplicationName = "";
-            this.ApplicationParameters = "";
-            this.ApplicationWorkingDirectory = "";
-            this.AppWindowHandle = this.m_AppWin;
-            this.ExternalProcessCaptured = (this.m_AppWin != IntPtr.Zero);
         }
 
         void Settings_SettingsSaving(object sender, CancelEventArgs e)
@@ -491,7 +490,7 @@ DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
             base.OnResize(e);
         }
 
-        public bool ExternalProcessCaptured;
+        public bool ExternalProcessCaptured { get { return this.m_AppWin != IntPtr.Zero; } }
 
         #endregion    
     

SuperPutty/ctlPuttyPanel.cs
@@ -66,8 +66,6 @@ namespace SuperPutty
             this.TextOverride = session.SessionName;
 
             CreatePanel();
-            this.Session = this.m_Session;
-            this.AppPanel = this.m_AppPanel;
             AdjustMenu();
         }
 
@@ -391,8 +389,8 @@ namespace SuperPutty
             }
         }
 
-        public SessionData Session;
-        public ApplicationPanel AppPanel;
+        public SessionData Session { get { return this.m_Session; } }
+        public ApplicationPanel AppPanel { get { return this.m_AppPanel; } }
         public ctlPuttyPanel previousPanel { get; set; }
         public ctlPuttyPanel nextPanel { get; set; }
 

SuperPutty/frmSuperPutty.cs
@@ -1165,8 +1165,8 @@ namespace SuperPutty
                         ctlPuttyPanel panel = doc as ctlPuttyPanel;
                         if (this.sendCommandsDocumentSelector.IsDocumentSelected(panel))
                         {
-                            System.IntPtr tmp = panel.AppPanel.AppWindowHandle;
-                            int handle = tmp.ToInt32();
+                            System.IntPtr hPtr = panel.AppPanel.AppWindowHandle;
+                            int handle = hPtr.ToInt32();
                             //Log.InfoFormat("SendCommand: session={0}, command=[{1}], handle={2}", panel.Session.SessionId, command, handle);
 
                             command.SendToTerminal(handle);