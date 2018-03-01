a9f6266b846faaca56cb1e2b3c8f80c820f53f3e
Jim Radford <jradford@npl.com>
2018-03-01 11:59:07 -0800

    Add action setting for RenameTab shortcut Closes Issue #693

--- SuperPutty/Properties/Settings.Designer.cs
+++ SuperPutty/Properties/Settings.Designer.cs
@@ -777,5 +777,18 @@ namespace SuperPutty.Properties {
                 this["CommandBarHistory"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Configuration.SettingsProviderAttribute(typeof(SuperPutty.Utils.PortableSettingsProvider))]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("None")]
+        public global::System.Windows.Forms.Keys Action_RenameTab_Shortcut {
+            get {
+                return ((global::System.Windows.Forms.Keys)(this["Action_RenameTab_Shortcut"]));
+            }
+            set {
+                this["Action_RenameTab_Shortcut"] = value;
+            }
+        }
     }
 }
