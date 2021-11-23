0762d0459ff264343c4099166618e0eaaa750c88
Jim Radford <jradford@npl.com>
2021-11-23 06:27:16 -0800

    Update log4net library to 2.0.13 See Issue #840, Issue #824

--- SuperPutty/Log4netDebugLogForm.cs
+++ SuperPutty/Log4netDebugLogForm.cs
@@ -26,7 +26,7 @@ namespace SuperPutty
             // start pulling log messages
             this.timerLogPull.Start();
 
-            Log.Info("Viewer Ready (" + log4net.AssemblyInfo.Version + ")");
+            Log.Info("Viewer Ready");
         }
 
         /// <summary>
